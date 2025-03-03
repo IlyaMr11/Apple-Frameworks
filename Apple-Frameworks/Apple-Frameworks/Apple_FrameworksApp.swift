c6542514971257228505717194b867ef95b27133
Ilya Morozov <ilya112006m@yandex.ru>
2025-03-03 19:12:48 +0300

    create project

---
+++ Apple-Frameworks/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Илья Морозов on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
